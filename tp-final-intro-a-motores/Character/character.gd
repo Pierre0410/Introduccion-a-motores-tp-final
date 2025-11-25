f62d7c8950ace60b79fbdf1b609ecdf4495fefca
Pierre0410 <pierregutierrez2004@gmail.com>
2025-11-25 18:18:09 -0300

    Terminado menu de pausa y de niveles

--- tp-final-intro-a-motores/Character/character.gd
+++ tp-final-intro-a-motores/Character/character.gd
@@ -2,6 +2,26 @@ extends CharacterBody2D
 
 @export var area_2d: Area2D
 # Called when the node enters the scene tree for the first time.
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -800
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+
 func _ready() -> void:
 	area_2d.body_entered.connect(_on_area_2d_body_entered)
 
---
+++ tp-final-intro-a-motores/Script/game_manager.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var score = 0
+
+func _ready() -> void:
+	pass
+	
+func add_Score():
+	score = score + 1
+
+func removeOne_Score():
+	score = score - 1
