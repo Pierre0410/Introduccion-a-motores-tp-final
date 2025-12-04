f17e00b6f6db86013fe7a40b5e2f290aaa3729ee
Pierre0410 <pierregutierrez2004@gmail.com>
2025-12-03 21:09:30 -0300

    ..

--- tp-final-intro-a-motores/Character/man_pac_level_2.gd
+++ tp-final-intro-a-motores/Character/man_pac_level_2.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var area_2d: Area2D
-@export var SPEED = 300.0
+@export var SPEED = 150.0
 @export var animacion: AnimatedSprite2D  
 @export var reproductor:AudioStreamPlayer2D
 
