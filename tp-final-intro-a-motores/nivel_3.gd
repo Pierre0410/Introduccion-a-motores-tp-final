e32a5892d79a87060c75df7e832d1ce2bc956f5a
Pierre0410 <pierregutierrez2004@gmail.com>
2025-12-04 18:01:13 -0300

    .

--- tp-final-intro-a-motores/nivel_3.gd
+++ tp-final-intro-a-motores/nivel_3.gd
@@ -5,3 +5,8 @@ extends Node2D
 func _ready() -> void:
 	GameManager.nivelActual = get_tree().current_scene.scene_file_path
 	print("se guardo el nivel 3")
+
+
+func _on_man_pac_personaje_muerto() -> void:
+	get_tree().change_scene_to_file(GameManager.nivelActual)
+	GameManager.reiniciarMarcador()
